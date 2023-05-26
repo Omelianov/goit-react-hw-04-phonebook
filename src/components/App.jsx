78b391e1d86e825a8daffac254705307fb12c4fa
Omelianov Nicolai <omelianov.n@gmail.com>
2023-05-26 17:24:36 +0300

    divide state in to two parts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,64 +5,50 @@ import AddForm from './AddForm/AddForm';
 import css from './App.module.css';
 
 const App = () => {
-  const [phonebook, setPhonebook] = useState(() => {
+  const [contacts, setContacts] = useState(() => {
     const savedData = localStorage.getItem('contacts');
-    return savedData ? JSON.parse(savedData) : {
-      contacts: [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ],
-      filter: '',
-    };
+    return savedData
+      ? JSON.parse(savedData).contacts
+      : [
+          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ];
   });
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(phonebook));
-  }, [phonebook]);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    const savedData = localStorage.getItem('contacts');
-    if (savedData!==null) {
-      setPhonebook(JSON.parse(savedData));
-    }
-  }, []);
-
+    localStorage.setItem('contacts', JSON.stringify({ contacts, filter }));
+  }, [contacts, filter]);
 
   const addContact = newContact => {
-    const findContact = phonebook.contacts.find(
+    const findContact = contacts.find(
       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
     if (findContact) {
       alert(`${newContact.name} is already in contacts`);
     } else {
-      setPhonebook(prevState => ({
-        ...prevState,
-        contacts: [...prevState.contacts, newContact],
-      }));
+      setContacts(prevContacts => [...prevContacts, newContact]);
     }
   };
 
   const handleFilter = event => {
-    setPhonebook({ ...phonebook, filter: event.target.value });
+    setFilter(event.target.value);
   };
 
   const renderContacts = () => {
-    const { contacts, filter } = phonebook;
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.trim().toLowerCase())
     );
   };
 
   const deleteContact = id => {
-    setPhonebook(prevState => ({
-      ...prevState,
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
   };
-
-  const { contacts, filter } = phonebook;
 
   return (
     <div className={css.container}>
